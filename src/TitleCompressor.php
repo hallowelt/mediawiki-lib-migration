4b7b08cba7084b719580f159a1e5e38dc4625ed5
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-12-11 09:12:13 +0100

    Working on title compressor

---
+++ src/TitleCompressor.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace HalloWelt\MediaWiki\Lib\Migration;
+
+use Exception;
+
+/**
+ * Compress titles with more then 255 chars
+ */
+class TitleCompressor {
+
+	private $generatedTitles = [];
+
+	private $longestTitles = [];
+
+	private $compressedTitles = [];
+
+	/**
+	 * @param array $pagesTitlesMap
+	 * @return array
+	 */
+	public function execute( array $pagesTitlesMap ): array {
+		foreach ( $pagesTitlesMap as $confluenceKey => $title ) {
+			$this->generatedTitles[] = $title;
+		}
+		// sort decending. Longest titles have lower index.
+		rsort( $this->generatedTitles );
+
+		$this->findLongestTitles();
+		$this->compressTitles();
+
+		return $this->compressedTitles;
+		// TODO: Replace titles in all global buckets
+	}
+
+	/**
+	 * @return void
+	 */
+	private function findLongestTitles(): void {
+		$curLongestTitle = '';
+		foreach ( $this->generatedTitles as $title ) {
+			if ( $curLongestTitle === '' ) {
+				// initial title
+				$curLongestTitle = $title;
+				continue;
+			}
+
+			if ( substr( $curLongestTitle, 0, strlen( $title ) ) === $title ) {
+				continue;
+			}
+
+			$this->longestTitles[] = $curLongestTitle;
+			$curLongestTitle = $title;
+		}
+		$this->longestTitles[] = $curLongestTitle;
+	}
+
+	/**
+	 * @return void
+	 */
+	private function compressTitles(): void {
+		for ( $index = 0; $index < count( $this->longestTitles ); $index++ ) {
+			$title = $this->longestTitles[$index];
+			$titleSegments = explode( '/', trim( $title, "/_ " ) );
+			
+			$numOfSegments = count( $titleSegments );
+			if ( $numOfSegments < 1 ) {
+				continue;
+			} elseif ( $numOfSegments > 1 ) {
+				// Title has title segments between root an leaf part or
+				// Title has root an leaf part only
+				$this->compressTitleWith2SegmentsAndMore( $titleSegments );
+			} else {
+				// Title has root part only
+				$this->compressTitleWith1Segment( $titleSegments );
+			}
+		}
+	}
+
+	/**
+	 * @param array $titleSegments
+	 * @return void
+	 */
+	private function compressTitleWith1Segment( array $titleSegments ): void {
+		$rootTitle = array_shift( $titleSegments );
+		$namespaceLength = strpos( $rootTitle, ':' );
+		$availableLength = 255 + $namespaceLength;
+		$compressedRootTitle = $this->compressTitle( '', $rootTitle, $availableLength );
+
+		$this->compressedTitles[$rootTitle] = $compressedRootTitle;
+	}
+
+	/**
+	 * @param array $titleSegments
+	 * @return void
+	 */
+	private function compressTitleWith2SegmentsAndMore( array $titleSegments ): void {
+		$numOfSegments = count( $titleSegments );
+
+		$namespaceLength = strpos( $titleSegments[0], ':' );
+		$namespace = substr( $titleSegments[0], 0, $namespaceLength );
+		$titleSegments[0] = substr( $titleSegments[0], $namespaceLength + 1 );
+
+		$availableLength = 255;
+		$allowedLeafPageLength = (int)($availableLength / $numOfSegments);
+		$leafPageTitle = array_pop( $titleSegments );
+
+		$availableLength = 255 - strlen( $allowedLeafPageLength );
+		$allowedRootLenght = (int)($availableLength / $numOfSegments);
+		$rootTitle = array_shift( $titleSegments );
+		$rootTitle = "{$namespace}:{$rootTitle}";
+
+		if ( strlen( $rootTitle ) > $allowedRootLenght ) {
+			// Compress $rootTitle
+			$compressedRootTitle = $this->compressTitle( '', $rootTitle, $allowedRootLenght + strlen( "{$namespace}:" ) );
+		} else {
+			$compressedRootTitle = $rootTitle;
+		}
+
+		if ( strlen( $leafPageTitle ) > $allowedLeafPageLength ) {
+			// Compress $leafPageTitle
+			$compressedLeafPageTitle = $this->compressTitle( '', $leafPageTitle, $allowedLeafPageLength );
+		} else {
+			$compressedLeafPageTitle = $leafPageTitle;
+		}
+
+		$numOfSegments = count( $titleSegments );
+		if ( $numOfSegments > 1 ) {
+			// Avoid "division by zero"
+			$availableLength = 255 - strlen( $compressedRootTitle ) - strlen( $compressedLeafPageTitle );
+			$segmentLength = (int)($availableLength / $numOfSegments);
+
+			$curTitle = $rootTitle;
+			$this->compressedTitles[$rootTitle] = $compressedRootTitle;
+			$compressedTitle = '';
+			foreach ( $titleSegments as $titleSegment ) {
+				$titleKey = "{$curTitle}";
+				if ( $titleKey === '' ) {
+					$titleKey = $titleSegment;
+				} else {
+					$titleKey .= "/{$titleSegment}";
+				}
+				if ( isset( $this->compressedTitles[$titleKey] ) ) {
+					$curTitle = $titleKey;
+					continue;
+				}
+
+				$compressedTitle = $this->compressTitle( $curTitle, $titleSegment, $segmentLength );
+				if ( $titleKey !== $compressedTitle ) {
+					$this->compressedTitles[$titleKey] = $compressedTitle;
+				}
+				$curTitle .= "/{$titleSegment}";
+			}
+
+			// Apend $leafPageTitle
+			if ( $leafPageTitle !== '' ) {
+				$curTitle .= "/{$leafPageTitle}";
+				$compressedTitle .= "/{$compressedLeafPageTitle}";
+				if ( $curTitle !== $compressedTitle ) {
+					$this->compressedTitles[$curTitle] = $compressedTitle;
+				}
+			}
+		} else {
+			$curTitle = $rootTitle;
+			$this->compressedTitles[$curTitle] = $compressedRootTitle;
+			$curTitle = "{$rootTitle}/{$leafPageTitle}";
+			$this->compressedTitles[$curTitle] = "{$compressedRootTitle}/{$compressedLeafPageTitle}";
+		}
+	}
+
+	/**
+	 * @param string $curTitle
+	 * @param string $titleSegment
+	 * @param int $segmentLength
+	 * @return string
+	 */
+	private function compressTitle( string $curTitle, string $titleSegment, int $segmentLength ): string {
+		$compressedCurTitle = '';
+		if ( isset( $this->compressedTitles[$curTitle] ) ) {
+			$compressedCurTitle = $this->compressedTitles[$curTitle];
+		}
+
+		if ( $compressedCurTitle !== '' ) {
+			$compressedCurTitle .= "/";
+		}
+		
+		// TODO: Throw exception if not set
+		$compressedTitle = '';
+		$titleCounter = 0;
+		$kill = 0;
+		do {
+			$titleCounter++;
+			$kill++;
+
+			// Segment length < $segmentLength => don't compress segment
+			// Segment length > $segmentLength => compress segment
+			if ( strlen( $titleSegment ) <= $segmentLength ) {
+				$compressedTitle = "{$compressedCurTitle}{$titleSegment}";
+				break;
+			} else {
+				$counter = "~" . (string)$titleCounter;
+				$counterLength = strlen( $counter );
+
+				$compressedTitleSegment = substr( $titleSegment, 0, $segmentLength - $counterLength );
+				$compressedTitleSegment .= $counter;
+
+				$compressedTitle = "{$compressedCurTitle}{$compressedTitleSegment}";
+			}
+		} while ( $kill <= 1000 && in_array( $compressedTitle, $this->compressedTitles ) );
+
+		if ( $kill > 1000 ) {
+			throw new Exception( 'To many loopls in TitleCompressor.' );
+		}
+
+		return $compressedTitle;
+	}
+}
---
+++ tests/phpunit/TitleCompressorTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace HalloWelt\MediaWiki\Lib\Migration\Tests;
+
+use HalloWelt\MediaWiki\Lib\Migration\TitleCompressor;
+use PHPUnit\Framework\TestCase;
+
+class TitleCompressorTest extends TestCase {
+
+	/**
+	 * @covers HalloWelt\MediaWiki\Lib\Migration\TitleCompressor::build
+	 * @return void
+	 */
+	public function testExecute() {
+		$titleCompressor = new TitleCompressor();
+
+		$pagesTitlesMap = $this->getPagesTitlesMap();
+		$compressedTitles = $titleCompressor->execute( $pagesTitlesMap );
+
+		$this->assertEquals(
+			$this->getExpectedCompressedTitleMap(),
+			$compressedTitles
+		);
+	}
+
+	private function getPagesTitlesMap(): array {
+		return [
+			'123456701---long root'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator_because_this_would_take_a_lot_of_time_sometimes',
+			'123456702---one subpage, short root'
+				=> 'ABC:A_very_very_very_long_root_page_title/'
+					. 'exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator_because_this_would_take_a_lot_of_time_sometimes',
+			'123456703---one subpage, long root'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator/'
+					. 'because_this_would_take_a_lot_of_time_sometimes',
+			'123456704---more then 2 segments'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php/'
+					. 'which_should_reduce_manual_title_modification/'
+					. 'after_analyze_process_by_human_operator/'
+					. 'because_this_would_take_a_lot_of_time_sometimes',
+			'123456705---more then 2 segments'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php part 2/'
+					. 'which_should_reduce_manual_title_modification/'
+					. 'after_analyze_process_by_human_operator but a very very very much longer subpage title/'
+					. 'because_this_would_take_a_lot_of_time_sometimes',
+		];	
+	}
+
+	private function getExpectedCompressedTitleMap(): array {
+		return [
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator_because_this_would_take_a_lot_of_time_sometimes'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator_because_this_would_ta~1',
+			'ABC:A_very_very_very_long_root_page_title'
+				=> 'ABC:A_very_very_very_long_root_page_title',
+			'ABC:A_very_very_very_long_root_page_title/'
+			. 'exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator_because_this_would_take_a_lot_of_time_sometimes'
+				=> 'ABC:A_very_very_very_long_root_page_title/'
+					. 'exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduc~1',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration~1',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php_which_should_reduce_manual_title_modification_after_analyze_process_by_human_operator/'
+			. 'because_this_would_take_a_lot_of_time_sometimes'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255_characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration~1/'
+					. 'because_this_would_take_a_lot_of_time_sometimes',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~1',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php/'
+			. 'which_should_reduce_manual_title_modification'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+				. 'characters_to_test_title_compression_algorithm_of~1/'
+				. 'which_should_reduce_manual_title_modification',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php/'
+			. 'which_should_reduce_manual_title_modification/'
+			. 'after_analyze_process_by_human_operator'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~1/'
+					. 'which_should_reduce_manual_title_modification/'
+					. 'after_analyze_process_by_human_operator',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php/'
+			. 'which_should_reduce_manual_title_modification/'
+			. 'after_analyze_process_by_human_operator/'
+			. 'because_this_would_take_a_lot_of_time_sometimes'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~1/'
+					. 'which_should_reduce_manual_title_modification/'
+					. 'after_analyze_process_by_human_operator/'
+					. 'because_this_would_take_a_lot_of_time_sometimes',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php part 2'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~2',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php part 2/'
+			. 'which_should_reduce_manual_title_modification'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~2/'
+					. 'which_should_reduce_manual_title_modification',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php part 2/'
+			. 'which_should_reduce_manual_title_modification/'
+			. 'after_analyze_process_by_human_operator but a very very very much longer subpage title'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~2/'
+					. 'which_should_reduce_manual_title_modification/'
+					. 'after_analyze_process_by_human_operator but a ver~1',
+			'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+			. 'characters_to_test_title_compression_algorithm_of_mediawiki-lib-migration_TitleCompressor.php part 2/'
+			. 'which_should_reduce_manual_title_modification/'
+			. 'after_analyze_process_by_human_operator but a very very very much longer subpage title/'
+			. 'because_this_would_take_a_lot_of_time_sometimes'
+				=> 'ABC:A_very_very_very_long_root_page_title_exeeding_255/'
+					. 'characters_to_test_title_compression_algorithm_of~2/'
+					. 'which_should_reduce_manual_title_modification/'
+					. 'after_analyze_process_by_human_operator but a ver~1/'
+					. 'because_this_would_take_a_lot_of_time_sometimes',
+
+		];
+	}
+}
